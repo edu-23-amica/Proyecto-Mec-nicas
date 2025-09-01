d2e14206cc2b897179022b898fa2b460950677ad
edu-23-amica <0264528@up.edu.mx>
2025-08-31 21:23:43 -0600

    Movimiento player + disparos

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject pooler;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MainPlayer>() != null)
+        {
+            gameObject.SetActive(false);
+            pooler.GetComponent<FireBallPooling>().FillObjects();
+        }
+    }
+}
