26bee132367e3ac29a175bdd81427b785903a3fb
h8m0ss <danarmenta32@gmail.com>
2025-09-03 09:32:41 -0600

    Created player health script

---
+++ Assets/Scripts/player/PlayerHealth.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    private int health;
+
+    public int MaxHealth { get => maxHealth; }
+    public int Health { get => health; }
+
+    public delegate void OnDeath();
+    public OnDeath onDeath;
+    public delegate void OnHurt();
+    public OnDeath onHurt;
+
+    void Start()
+    {
+        health = maxHealth;
+        onDeath += ActionsOnDeath;
+    }
+
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Player health is now: " + health);
+        onHurt?.Invoke();
+        if (health <= 0)
+        {
+            onDeath?.Invoke();
+        }
+    }
+
+    void ActionsOnDeath()
+    {
+        // Desactivar al main player al morir
+        // gameObject.SetActive(false);
+    }
+}
