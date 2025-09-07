0a298f1ea71831a960159c142599d653b6f9cb08
Ale Nava <sebasesmn@gmail.com>
2025-09-07 13:24:47 -0600

    Boss Final
    Agregué toda la escena del boss final, no se como sera el escenario pero el boss tiene la opcion de ponerle x puntos de Teleport para acomodarlo facil.

---
+++ Assets/ScriptsBoss/Bullet.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float tiempoBala = 3f;
+    public int daño = 2;
+    //public PlayerHealth ph;
+    void Start()
+    {
+        Destroy(gameObject, tiempoBala);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            //Esta es la funcion para hacer daño al player pero como no tengo el script la tengo comentada
+            //ph.DealDamage(daño);
+        }
+
+    }
+}
